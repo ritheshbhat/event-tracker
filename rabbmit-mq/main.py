5e1fda7ea91eff2fd10a8651f5dee94b12d673c6
Rithesh <bhatrithesh@yahoo.com>
2022-11-08 17:34:53 -0600

    mq configurations

--- rabbmit-mq/main.py
+++ rabbmit-mq/main.py
@@ -3,18 +3,16 @@ import pika
 def send_message():
     credentials = pika.PlainCredentials(username='guest', password='guest')
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, credentials= credentials))
+    connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost', port=5671, credentials=credentials))
     channel = connection.channel()
-    channel.queue_declare(queue='sports')
-    channel.basic_publish(exchange='sports',
-                          routing_key='',
-                          body='Hello World!')
+
+    channel.queue_declare(queue='hello')
+
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
     print(" [x] Sent 'Hello World!'")
     connection.close()
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     send_message()
-# #
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-# example_publisher.py
---
+++ rabbmit-mq/readme.md
@@ -0,0 +1,6 @@
+3.11.2-management rabbitmq versiomn
+
+
+host port: 15672, 5672
+
+ docker run --rm -it --hostname my-rabit -p 15672:15672 -p 5672:5672 rabbitmq:3.11.2-management
