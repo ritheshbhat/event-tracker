4f88e836d6b8251c76df5910ace4c0a2de670209
Rithesh <bhatrithesh@yahoo.com>
2022-11-08 22:50:04 -0600

    automatic queue generation for consumers with exchange name same as department name

--- rabbmit-mq/consumer.py
+++ rabbmit-mq/consumer.py
@@ -8,13 +8,16 @@ def consume():
     connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost', port=5671, credentials=credentials))
     channel = connection.channel()
+    channel.exchange_declare(exchange='logs', exchange_type='fanout')
+    result = channel.queue_declare(queue='', exclusive=True)
+    queue_name = result.method.queue
 
-    channel.queue_declare(queue='hello')
+    channel.queue_bind(exchange='logs', queue=queue_name)
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- rabbmit-mq/main.py
+++ rabbmit-mq/main.py
@@ -1,18 +1,46 @@
 import pika
+from pika.adapters.blocking_connection import BlockingChannel
 
-def send_message():
-    credentials = pika.PlainCredentials(username='guest', password='guest')
 
-    connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost', port=5671, credentials=credentials))
-    channel = connection.channel()
+# def send_message():
+#     credentials = pika.PlainCredentials(username='guest', password='guest')
+#
+#     connection = pika.BlockingConnection(
+#         pika.ConnectionParameters(host='localhost', port=5671, credentials=credentials))
+#     channel = connection.channel()
+#
+#     channel.queue_declare(queue='hello')
+#
+#     channel.basic_publish(exchange='', routing_key='hello', body='sending second request.')
+#     print(" [x] Sent 'Hello World!'")
+#     connection.close()
+#
 
-    channel.queue_declare(queue='hello')
+class RabbiMQManager:
+    def __init__(self, host: str, port: int, username: str, password: str):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.channel: BlockingChannel = None
 
-    channel.basic_publish(exchange='', routing_key='hello', body='sending second request.')
-    print(" [x] Sent 'Hello World!'")
-    connection.close()
+    def setup_connection_and_channel(self) -> BlockingChannel:
+        credentials = pika.PlainCredentials(username=self.username, password=self.password)
+
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.host, port=self.port, credentials=credentials))
+        channel = connection.channel()
+        self.channel = channel
+        return channel
+
+    def publish_message(self, queue: str, message: str):
+        self.channel.exchange_declare(exchange='logs',
+                                      exchange_type='fanout')
+        #
+        self.channel.basic_publish(exchange='logs', routing_key='', body=message)
+
 
 if __name__ == '__main__':
-    send_message()
-
+    r = RabbiMQManager('localhost', 5671, 'guest', 'guest')
+    r.setup_connection_and_channel()
+    r.publish_message("music", "testing-music-data")
