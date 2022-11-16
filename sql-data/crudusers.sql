b9cc0c53507cee5e3274c1cfb653decf94b1219d
anushakp31 <anushakp31@gmail.com>
2022-11-16 16:50:48 -0600

    Anusha | Modify sql file

--- sql-data/crudusers.sql
+++ sql-data/crudusers.sql
@@ -3,13 +3,15 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2022 at 08:27 AM
+-- Generation Time: Nov 16, 2022 at 11:49 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET
+SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET
+time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -24,82 +26,99 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `departments`
 --
 
-CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL,
-  `age` int(11) DEFAULT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `occupation` varchar(255) DEFAULT NULL
+CREATE TABLE `departments`
+(
+    `id`   bigint(20) NOT NULL,
+    `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `departments`
 --
 
-INSERT INTO `user` (`id`, `age`, `first_name`, `last_name`, `occupation`) VALUES
-(1, 20, 'John', 'Henderson', 'Student'),
-(3, 0, NULL, NULL, NULL),
-(4, 0, NULL, NULL, NULL),
-(5, 23, 'Jessica', 'Day', 'Teacher');
+INSERT INTO `departments` (`id`, `name`)
+VALUES (1, ''Computer Science''),
+       (2, ''Engineering Management''),
+       (3, ''Aeronautical Engineering''),
+       (4, ''Department of Nursing''),
+       (5, ''Biology''),
+       (6, ''Physics''),
+       (7, ''Civil''),
+       (8, ''Mechanical'');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `events`
 --
 
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL,
-  `age` int(11) DEFAULT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `logged_in` bit(1) NOT NULL,
-  `occupation` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL
+CREATE TABLE `events`
+(
+    `event_id`      bigint(20) NOT NULL,
+    `description`   varchar(255) NOT NULL,
+    `event_date`    date         NOT NULL,
+    `event_time`    time         NOT NULL,
+    `title`         varchar(255) NOT NULL,
+    `venue`         varchar(255) NOT NULL,
+    `department_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `events`
 --
 
-INSERT INTO `users` (`id`, `age`, `first_name`, `last_name`, `logged_in`, `occupation`, `password`, `username`) VALUES
-(1, 0, NULL, NULL, b'0', NULL, 'test', 'test');
+INSERT INTO `events` (`event_id`, `description`, `event_date`, `event_time`, `title`, `venue`, `department_id`)
+VALUES (1, ''Paint'', ''2022 - 10 - 19 '', ''12:00:00'', ''Halloween'', ''UC'', 6),
+       (2, ''Fall 2022 Career Fair'', ''2022 - 09 - 28 '', ''11:00:00'', ''Career Fair'', ''MAC'', 1),
+       (4, ''Fall 2022 Career Fair'', ''2022 - 09 - 28 '', ''11:00:00'', ''Career Fair'', ''MAC'', 6),
+       (5, ''Fall 2022 Career Fair'', ''2022 - 09 - 28 '', ''11:00:00'', ''t'', ''MAC'', 6),
+       (6, ''Paint'', ''2022 - 10 - 19 '', ''11:00:00'', ''Halloween'', ''UC'', 5);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `user`
+-- Indexes for table `departments`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `departments`
+    ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `events`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `events`
+    ADD PRIMARY KEY (`event_id`),
+  ADD KEY `FK2oib4sxiq6yfydyf20mopjqh4` (`department_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `departments`
 --
-ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `departments`
+    MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `events`
 --
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `events`
+    MODIFY `event_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+    ADD CONSTRAINT `FK2oib4sxiq6yfydyf20mopjqh4` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
